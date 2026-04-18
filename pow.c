b43c6973cf7bebfef9f8abd6fe2f8c7a1d6118aa
S$Y <syedyasser728@gmail.com>
2026-04-19 01:11:00 +0530

    Added the problem statement in a comment

--- pow.c
+++ pow.c
@@ -1,6 +1,18 @@
+//Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
 double myPow(double x, int n)
 {
     double res;
     res = pow(x, n);
     return res;
 }
+
+/*Input 1: x = 2.00000, n = 10
+Output 1: 1024.00000
+
+Input 2: x = 2.10000, n = 3
+Output 2: 9.26100
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+*/
