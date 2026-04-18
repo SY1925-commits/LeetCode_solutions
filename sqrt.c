dcb1abee28f21e49c9c1688b406e529080886bed
S$Y <syedyasser728@gmail.com>
2026-04-19 01:53:44 +0530

    4th solution

---
+++ sqrt.c
@@ -0,0 +1,15 @@
+/*Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
+The returned integer should be non-negative as well.*/
+
+int mySqrt(int x) 
+{
+    return sqrt(x);
+}
+
+/*
+Input 1: x = 4
+Output 1: 2
+
+Input 2: x = 8
+Output 2: 2
+*/
