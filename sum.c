a382cb196a1f833900255b9b1576d808b6609198
S$Y <syedyasser728@gmail.com>
2026-04-19 01:19:52 +0530

    Added the question and i/o as comments

--- sum.c
+++ sum.c
@@ -1,6 +1,16 @@
+//Given two integers num1 and num2, return the sum of the two integers.
+
 int sum(int num1, int num2) 
 {
     int sum;
     sum = num1 + num2;
     return sum;
 }
+
+/*
+Input 1: num1 = 12, num2 = 5
+Output 1: 17
+
+Input 2: num1 = -10, num2 = 4
+Output 2: -6
+*/
